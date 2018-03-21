d4d2f14153925451b326338e2e0fb88e89fd0845
Marcin Ruszkiewicz <marcin.ruszkiewicz@polcode.net>
2018-03-21 08:50:20 +0100

    Add code documentation, bump up minor gem version
    Resolves #2

--- lib/active_record/where_chain_shared_methods.rb
+++ lib/active_record/where_chain_shared_methods.rb
@@ -3,16 +3,18 @@ module ActiveRecord
     extend ActiveSupport::Concern
 
     included do
+      # Initialize the chain with a scope and a toggle to invert the generated
+      # where statement
       def initialize(scope, invert=false)
         @scope = scope
         @invert = invert
       end
-            
+
       # Returns a new relation expressing WHERE + LIKE condition
       # according to the conditions provided as a hash in the arguments.
       #
       #    Book.where.like(title: "Rails%")
-      #    # SELECT * FROM books WHERE title LIKE 'Rails%'
+      #    # SELECT * FROM books WHERE books.title LIKE 'Rails%'
       def like(opts, *rest)
         prepare_where(Arel::Nodes::Matches, nil, opts, rest)
       end
@@ -21,51 +23,77 @@ module ActiveRecord
       # according to the conditions provided as a hash in the arguments.
       #
       #    Conference.where.not_like(name: "%Kaigi")
-      #    # SELECT * FROM conferences WHERE name NOT LIKE '%Kaigi'
+      #    # SELECT * FROM conferences WHERE conferences.name NOT LIKE '%Kaigi'
+      #
+      # Also aliased as .not_like to maintain compatibility with the
+      # activerecord-like gem
       def unlike(opts, *rest)
         prepare_where(Arel::Nodes::DoesNotMatch, nil, opts, rest)
       end
-      alias not_like unlike # maintain compatibility with activerecord-like gem
+      alias not_like unlike
 
+      # Returns a new relation expressing a greater than condition in WHERE
+      # according to the conditions in an argument hash
+      #
+      #    Conference.where.gt(speakers: 10)
+      #    # SELECT * FROM conferences WHERE conferences.speakers > 10
       def gt(opts, *rest)
         ensure_proper_attributes(opts)
         prepare_where(Arel::Nodes::InfixOperation, '>', opts, rest)
       end
 
+      # Returns a new relation expressing a greater than or equal condition in WHERE
+      # according to the conditions in an argument hash
+      #
+      #    Conference.where.gte(speakers: 10)
+      #    # SELECT * FROM conferences WHERE conferences.speakers >= 10
       def gte(opts, *rest)
         ensure_proper_attributes(opts)
         prepare_where(Arel::Nodes::InfixOperation, '>=', opts, rest)
       end
 
+      # Returns a new relation expressing a less than condition in WHERE
+      # according to the conditions in an argument hash
+      #
+      #    Conference.where.lt(date: DateTime.new(2018, 3, 18))
+      #    # SELECT * FROM conferences WHERE conferences.date < '2018-03-18 00:00:00'
       def lt(opts, *rest)
         ensure_proper_attributes(opts)
         prepare_where(Arel::Nodes::InfixOperation, '<', opts, rest)
       end
 
+      # Returns a new relation expressing a less than or equal condition in WHERE
+      # according to the conditions in an argument hash
+      #
+      #    Conference.where.lt(date: DateTime.new(2018, 3, 18))
+      #    # SELECT * FROM conferences WHERE conferences.date <= '2018-03-18 00:00:00'
       def lte(opts, *rest)
         ensure_proper_attributes(opts)
         prepare_where(Arel::Nodes::InfixOperation, '<=', opts, rest)
       end
 
-      private 
+      private
 
+      # Ensures that the arguments passed to methods are a Hash and that the value in the argument
+      # hash is not another Hash or Array.
       def ensure_proper_attributes(opts)
         raise ArgumentError, 'This method requires a Hash as an argument.' unless opts.is_a?(Hash)
 
         opts.each_pair do |key, value|
           if value.is_a?(Hash) || value.is_a?(Array)
-            raise ArgumentError, 'The value passed to this method should be a valid type.' 
+            raise ArgumentError, 'The value passed to this method should be a valid type.'
           end
         end
       end
 
+      # Arel::Nodes::InfixOperation.new expects more arguments than other Arel nodes
       def arel_node(node_type, infix, rel)
         if infix.present?
           node_type.new(infix, rel.left, rel.right)
         else
           node_type.new(rel.left, rel.right)
         end
-      end      
+      end
     end
   end
 end
